9af601befd014ba8469d960a997dafc1b8d0740f
pearu.peterson <pearu.peterson@eb797620-833c-0410-b191-3188e4ee2b90>
2007-12-23 13:55:09 +0000

    Impl InstanceClassProperty decorator, BasicOperator and BasicParametricFunction classes.

--- research/InstanceClassMethod_decorator.py
+++ research/InstanceClassMethod_decorator.py
@@ -2,9 +2,11 @@ import new
 import types
 
 class InstanceClassMethod(object):
-    """ Decorator that maps a metaclass method and a classmethod of a base class
-    to a classmethod of a given class when the given class defines an instance
-    method.
+    """ Decorator that allows to use the same name for class
+    and instance methods.
+    
+    When calling a method via class object then metaclass method
+    or the classmethod of a base class with the same name will be used.
 
     Example
     -------
@@ -27,7 +29,7 @@ class AType(type):
 class A(object):
     __metaclass__ = AType
 
-    # metaclass method will be used as class method
+    # metaclass method will be used as class method:
     @InstanceClassMethod
     def foo(self):
         return self
@@ -41,7 +43,7 @@ class A(object):
 
 class B(A):
 
-    # classmethod is defined in a parent class
+    # classmethod is defined in a parent class:
     def bar(self):
         return self
     bar = InstanceClassMethod(bar, A.__dict__['bar'])
@@ -128,6 +130,32 @@ class B(A):
         return '%s(%r, %r, %r)' % (self.__class__.__name__,
                                self.method, self.clsmethod, self.clsname)
 
+class InstanceClassProperty(object):
+    """ Decorator similar to InstanceClassMethod but
+    applies to property methods and attributes.
+    """
+    
+    def __new__(cls, method, clsname):
+        assert isinstance(method, property), type(method)
+        obj = object.__new__(cls)
+        obj.method = method
+        obj.name = method.fget.__name__
+        obj.clsname = clsname
+        return obj
+
+    def __get__(self, obj, owner):
+        name = self.name
+        if obj is None:
+            return getattr(type(owner), name, self.method)
+        else:
+            return self.method.__get__(obj, owner)
+
+    def __repr__(self):
+        return '%s(%r, %r)' % (self.__class__.__name__,
+                               self.method, self.clsname)
+
+special_methods = ('__new__', '__init__')
+
 def apply_InstanceClassMethod_to_Class_methods(cls, d = None, Class=object):
     if d is None:
         d = {}
@@ -137,15 +165,16 @@ def apply_InstanceClassMethod_to_Class_methods(cls, d = None, Class=object):
         mro = cls.__mro__
     else:
         for k, v in cls.__dict__.iteritems():
+            if k in special_methods:
+                continue
             if k in d:
+                v1 = d[k]
                 if isinstance(v, classmethod):
-                    v1 = d[k]
                     if isinstance(v1, InstanceClassMethod) and v1.clsmethod is None:
                         d[k] = InstanceClassMethod(v1.method, v, v1.clsname + ',' + cls.__name__)
                 elif isinstance(v, InstanceClassMethod):
-                    v1 = d[k]
-                    if isinstance(v1, classmethod) and v.clsmethod is None:
-                        d[k] = InstanceClassMethod(v.method, v1, cls.__name__ + ',' + v.clsname)
+                    if isinstance(v1, classmethod):# and v.clsmethod is None:
+                        d[k] = InstanceClassMethod(v.method, v1)#, cls.__name__ + ',' + v.clsname)
                 continue
             if isinstance(v, InstanceClassMethod):
                 d[k] = v
@@ -163,8 +192,13 @@ def new_type(basetype, name, bases=(), attrdict={}):
 
     d = {}
     for k, v in attrdict.iteritems():
+        if k in special_methods:
+            d[k] = v
+            continue
         if isinstance(v, types.FunctionType):
             d[k] = InstanceClassMethod(v, clsname = name)
+        elif isinstance(v, property):
+            d[k] = InstanceClassProperty(v, clsname = name)
         else:
             d[k] = v
 
@@ -174,6 +208,10 @@ def new_type(basetype, name, bases=(), attrdict={}):
     t = type.__new__(basetype, name, bases, d)
 
     return t
+
+#################################
+##### Unittests
+#################################
 
 class BasicType(type):
 
@@ -225,9 +263,21 @@ class Basic(object):
         assert isinstance(self, Basic)
         return 2
 
+    @property
+    def prop7(self):
+        return 2
+
+    prop8 = 2
+
 class BasicFunctionType(Basic, BasicType):
 
     __new__ = new_type # applies InstanceClassMethod to Basic methods
+
+    def __str__(self):
+        return self.__name__
+
+    def __repr__(self):
+        return type.__repr__(self)
 
     def __eq__(self, other):
         #print 'BasicFunctionType'
@@ -248,9 +298,44 @@ class BasicFunctionType(Basic, BasicType):
         assert isinstance(self, BasicType)
         return 3
 
+    @property
+    def prop1(self):
+        return 3
+
+    prop2 =3
+
+    @property
+    def prop3(self):
+        return 3
+
+    prop4 = 3
+
+    @property
+    def prop5(self):
+        return 3
+
 class BasicFunction(Basic):
 
     __metaclass__ = BasicFunctionType
+
+    def __new__(cls, *args):
+        obj = cls.canonize(args)
+        if obj is not None:
+            return obj
+        obj = object.__new__(cls)
+        obj.args = args
+        obj.func = cls
+        return obj
+
+    @classmethod
+    def canonize(cls, args):
+        return
+
+    def __str__(self):
+        return '%s(%s)' % (self.func, ', '.join(map(str, self.args)))
+
+    def __repr__(self):
+        return '%s(%s)' % (self.func, ', '.join(map(repr, self.args)))
 
     def __eq__(self, other):
         #print 'BasicFunction'
@@ -278,6 +363,125 @@ class BasicFunction(Basic):
     def Fun(self):
         return 4
 
+    @property
+    def prop1(self):
+        return 4
+
+    @property
+    def prop2(self):
+        return 4
+
+    prop3 = 4
+
+    prop4 = 4
+
+    @property
+    def prop6(self):
+        return 4
+
+def instancemethod(clsmth):
+    def wrap(func):
+        return InstanceClassMethod(func, clsmth)
+    return wrap
+
+class BasicOperator(BasicFunction):
+
+    is_metaoperator = True
+    is_operator = False
+    is_function = False
+
+    def __new__(cls, *args):
+        obj = cls.canonize(args)
+        if obj is not None:
+            return obj
+        if cls.is_metaoperator:
+            name = '%s(%s)' % (cls.__name__, ', '.join(map(str, args)))
+            obj = new_type(type(cls), name, bases = (cls, ),
+                         attrdict=dict(args=args,
+                                       is_metaoperator = False,
+                                       is_operator = True))
+            return obj
+        if cls.is_operator:
+            name = '%s(%s)' % (cls.__name__, ', '.join(map(str, args)))
+            obj = new_type(type(cls), name, bases = (cls, ),
+                         attrdict=dict(args=args,
+                                       is_operator = False,
+                                       is_function = True))
+            return obj
+        obj = object.__new__(cls)
+        obj.is_function = False
+        obj.args = args
+        obj.func = cls
+        return obj
+
+    @classmethod
+    def canonize(cls, args):
+        print cls.is_operator, cls.is_function, getattr(cls, 'args', None), args
+        return
+
+    @instancemethod(canonize)
+    def canonize(self, args):
+        print self.is_operator, self.is_function, self.args, args
+        return
+
+    def __str__(self):
+        return '%s(%s)' % (self.func, ', '.join(map(str, self.args)))
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__, ', '.join(map(repr, self.args)))
+
+class BasicParametricFunction(BasicFunction):
+
+    is_metafunction = True
+    is_function = False
+
+    def __new__(cls, *args):
+        obj = cls.canonize(args)
+        if obj is not None:
+            return obj
+
+        if cls.is_metafunction:
+            name = '%s(%s)' % (cls.__name__, ', '.join(map(str, args)))
+            obj = new_type(cls.__metaclass__, name, bases = (cls, ),
+                           attrdict=dict(args=args,
+                                         is_metafunction = False,
+                                         is_function = True))
+            return obj
+        obj = object.__new__(cls)
+        obj.is_function = False
+        obj.args = args
+        obj.func = cls
+        return obj
+
+    def __str__(self):
+        return self.__name__
+
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, ', '.join(map(repr, self.args)))
+
+    @classmethod
+    def canonize(cls, args):
+        print cls.is_function, cls.args, args
+        return
+
+    @instancemethod(canonize)
+    def canonize(self, args):
+        print cls.is_function, cls.args, args
+        return
+
+######### User defined classes
+
+
+class Hermite(BasicParametricFunction):
+
+    @classmethod
+    def canonize(cls, args):
+        return
+
+class D(BasicOperator):
+
+    pass
+
 class MyFunction(BasicFunction):
 
     def fun(self):
@@ -300,6 +504,14 @@ class MyFunction(BasicFunction):
 
     @classmethod
     def Fun(cls):
+        return 5
+
+    @property
+    def prop7(self):
+        return 5
+
+    @property
+    def prop8(self):
         return 5
 
 # Unittests:
@@ -378,3 +590,51 @@ assert MyFunction.Bar()==5.1
 # Fun is defined in MyFunction as classmethod but in BasicFunction as instance method
 assert MyFunction.Fun()==5
 assert MyFunction().Fun()==4
+
+# prop1 is a property defined in BasicFunctionType and BasicFunction
+
+assert BasicFunction.prop1==3
+assert BasicFunction().prop1==4
+assert MyFunction.prop1==3
+assert MyFunction().prop1==4
+
+# prop2 is defined in BasicFunction as property and in BasicFunctionType as attribute
+assert BasicFunction.prop2==3
+assert BasicFunction().prop2==4
+assert MyFunction.prop2==3
+assert MyFunction().prop2==4
+
+# prop3 is defined in BasicFunctionType as property and in BasicFunction as attribute
+assert BasicFunction.prop3==3
+assert BasicFunction().prop3==4
+assert MyFunction.prop3==3
+assert MyFunction().prop3==4
+
+# prop4 is defined in BasicFunctionType and in BasicFunction as attribute
+assert BasicFunction.prop4==4
+assert BasicFunction().prop4==4
+assert MyFunction.prop4==4
+assert MyFunction().prop4==4
+
+# prop5 is defined in BasicFunctionType as property
+assert BasicFunction.prop5==3
+assert MyFunction.prop5==3
+
+# prop6 is defined in BasicFunction as property
+assert BasicFunction().prop6==4
+assert MyFunction().prop6==4
+assert isinstance(BasicFunction.prop6, property)==True
+assert isinstance(MyFunction.prop6, property)==True
+
+# prop7 is defined in Basic, redefined in MyFunction
+assert Basic().prop7==2
+assert BasicFunction.prop7==2
+assert isinstance(BasicFunctionType.prop7, property)==True
+assert BasicFunction().prop7==2
+assert MyFunction().prop7==5
+
+# prop8 is defined in Basic as attribute, redefined in MyFunction as property
+assert Basic().prop8==2
+assert BasicFunction.prop8==2
+assert BasicFunction().prop8==2
+assert MyFunction().prop8==5
