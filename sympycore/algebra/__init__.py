11f7d1a00f77d9d44cc3e25db4eeb4c085c603bf
pearu.peterson <pearu.peterson@eb797620-833c-0410-b191-3188e4ee2b90>
2008-01-09 11:19:28 +0000

    Impl string to PrimitiveAlgebra parser - warns when something is unsupported.

--- sympycore/algebra/__init__.py
+++ sympycore/algebra/__init__.py
@@ -1,7 +1,5 @@
 
 from .algebraic_structures import AlgebraicStructure
+from .primitive import PrimitiveAlgebra
 from .std_commutative_algebra import Symbolic, SymbolicNumber, StandardCommutativeAlgebra
 from .pairs import CommutativePairs
-
-#from .basic import AlgebraicExpression, AlgebraicNumberExpression, as_ae
-#from .commutative import CommutativePairs
--- sympycore/algebra/primitive.py
+++ sympycore/algebra/primitive.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import
+import types
+import compiler
 
 from .algebraic_structures import AlgebraicStructure
 
@@ -15,6 +18,8 @@ class PrimitiveAlgebra(AlgebraicStructure):
     def __new__(cls, tree):
         if hasattr(tree, 'as_PrimitiveAlgebra'):
             tree = tree.as_PrimitiveAlgebra()
+        if isinstance(tree, str): # XXX: unicode
+            tree = string2PrimitiveAlgebra(tree)
         if isinstance(tree, cls):
             return tree
         if not isinstance(tree, tuple):
@@ -55,3 +60,74 @@ class PrimitiveAlgebra(AlgebraicStructure):
     def __rpow__(self, other): return PrimitiveAlgebra((POW, (other, self)))
     __truediv__ = __div__
     __rtruediv__ = __rdiv__
+
+
+########### string to PrimitiveAlgebra parser ############
+
+node_names = []
+skip_names = ['Module','Stmt','Discard']
+Node = compiler.ast.Node
+for n, cls in compiler.ast.__dict__.items():
+    if n in skip_names:
+        continue
+    if isinstance(cls, (type,types.ClassType)) and issubclass(cls, Node):
+        node_names.append(n)
+
+class PrimitiveWalker:
+
+    def __init__(self):
+        self.stack = []
+
+    # for composite instance:
+    def start(self, head):
+        self.stack.append([head, []])
+    def append(self, obj):
+        stack = self.stack
+        if not stack:
+            stack.append(obj)
+        else:
+            lst = stack[-1]
+            if isinstance(lst[1], list):
+                lst[1].append(obj)
+            else:
+                print "Cannot append %r to %s" % (obj, lst)
+    def end(self):
+        last = self.stack.pop()
+        print last
+        head, lst = last
+        self.append(PrimitiveAlgebra((head, tuple(lst))))
+    # for atomic instance:
+    def add(self, obj):
+        self.append(PrimitiveAlgebra(obj))
+
+    for _n in node_names:
+        exec '''\
+def visit%s(self, node, *args):
+    print "warning: using default visit%s"
+    self.start(%r)
+    for child in node.getChildNodes():
+        self.visit(child, *args)
+    self.end()
+''' % (_n, _n, _n)
+
+    # visitNode methods:
+    def visitName(self, node):
+        self.add((SYMBOL, node.name))
+
+    def visitConst(self, node):
+        self.add((NUMBER, node.value))
+
+    def visitAdd(self, node):
+        self.start(ADD)
+        self.visit(node.left)
+        self.visit(node.right)
+        self.end()
+    def visitMul(self, node):
+        self.start(MUL)
+        self.visit(node.left)
+        self.visit(node.right)
+        self.end()
+
+def string2PrimitiveAlgebra(expr):
+    node = compiler.parse(expr)
+    return compiler.walk(node, PrimitiveWalker()).stack.pop()
