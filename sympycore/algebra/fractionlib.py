c92dda71da671c362e782ef03ab03fa443e2679c
fredrik.johansson <fredrik.johansson@gmail.com>
2008-01-12 12:23:47 +0000

    add fractionlib

---
+++ sympycore/algebra/fractionlib.py
@@ -0,0 +1,123 @@
+"""
+The mpq fraction type implemented here is only intended to be used to
+extend code that would otherwise use Python ints for the purpose
+of representing exact numbers. Addition, multiplication and comparison
+with mpqs and Python ints is therefore safe.
+
+However, integer-valued fractions are converted back to Python ints,
+and therefore code that would not work ordinarily with ints like
+
+    a = mpq(2)
+    b = mpq(3)
+    a / b
+
+should not be expected to work (this itself is an implementation detail
+that may be changed). For this reason, non-integer-safe operations are
+provided as functions (which take either mpq or Python ints as input).
+
+Possible issues:
+* Hashing is not compatible with floats (however, mixing floats and
+  rationals is probably not a good idea).
+
+"""
+
+inttypes = (int, long)
+
+class mpq(tuple):
+
+    def __new__(cls, p, q=1, tnew=tuple.__new__):
+        x, y = p, q
+        while y:
+            x, y = y, x % y
+        if x != 1:
+            p //= x
+            q //= x
+        if q == 1:
+            return p
+        return tnew(cls, (p, q))
+
+    def __str__(self):
+        return "%i/%i" % self
+
+    def __repr__(self):
+        return "mpq(%i, %i)" % (self[0], self[1])
+
+    # not needed when __new__ normalizes to ints
+    # __nonzero__
+    # __eq__
+    # __hash__
+
+    def __float__(self):
+        p, q = self
+        return float(p) / q
+
+    def __neg__(self, tnew=tuple.__new__):
+        p, q = self
+        return tnew(rational, (-p, q))
+
+    def __pos__(self):
+        return self
+
+    def __add__(self, other, check=isinstance, inttypes=inttypes):
+        p, q = self
+        if check(other, inttypes):
+            r, s = other, 1
+        elif check(other, mpq):
+            r, s = other
+        else:
+            return NotImplemented
+        return mpq(p*s + q*r, q*s)
+
+    __radd__ = __add__
+
+    def __sub__(self, other, check=isinstance, inttypes=inttypes):
+        p, q = self
+        if check(other, inttypes):
+            r, s = other, 1
+        elif check(other, mpq):
+            r, s = other
+        else:
+            return NotImplemented
+        return mpq(p*s - q*r, q*s)
+
+    def __rsub__(self, other, check=isinstance, inttypes=inttypes):
+        p, q = self
+        if check(other, inttypes):
+            r, s = other, 1
+        elif check(other, mpq):
+            r, s = other
+        else:
+            return NotImplemented
+        return mpq(q*r - p*s, q*s)
+
+    def __mul__(self, other, check=isinstance, inttypes=inttypes):
+        p, q = self
+        if check(other, inttypes):
+            r, s = other, 1
+        elif check(other, mpq):
+            r, s = other
+        else:
+            return NotImplemented
+        return mpq(p*r, q*s)
+
+    __rmul__ = __mul__
+
+    def __pow__(self, n):
+        if not (isinstance(n, inttypes) and n >= 0):
+            raise ValueError
+        p, q = self
+        return mpq(p**n, q**n)
+
+rational_types = (int, long, mpq)
+
+def div(a, b, check=isinstance, inttypes=inttypes):
+    if check(a, inttypes):
+        p, q = a, 1
+    else:
+        p, q = a
+    if check(b, inttypes):
+        r, s = b, 1
+    else:
+        r, s = b
+    return mpq(p*s, q*r)
+
---
+++ sympycore/algebra/tests/test_fractionlib.py
@@ -0,0 +1,22 @@
+from sympycore.algebra.fractionlib import *
+
+def test_mpq():
+    assert mpq(1) == 1
+    assert mpq(1,1) == 1
+    assert mpq(1,2) != 1
+    assert mpq(1,2) == mpq(2,4)
+    assert mpq(1,2) + mpq(5,6) == mpq(4,3)
+    assert 2*mpq(1,2) == 1
+    assert mpq(1,2)*2 == 1
+    assert mpq(1,3) + mpq(2,3) == 1
+    assert float(mpq(1,4)) == 0.25
+    assert mpq(1,2) == mpq(-1,-2)
+    assert mpq(-1,2) == mpq(1,-2)
+    assert mpq(1,2) - 1 == mpq(-1,2)
+    assert 1 - mpq(1,2) == mpq(1,2)
+    assert mpq(2,3)**0 == 1
+    assert mpq(2,3)**1 == mpq(2,3)
+    assert mpq(2,3)**2 == mpq(4,9)
+    assert div(1,2) == mpq(1,2)
+    assert div(3,mpq(1,2)) == 6
+    assert div(mpq(1,2),mpq(3,2)) == mpq(1,3)
