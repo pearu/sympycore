5e9fc90fde4e62484ee43c067d727d185d7a2cd2
pearu.peterson <pearu.peterson@eb797620-833c-0410-b191-3188e4ee2b90>
2008-01-23 14:51:11 +0000

    Cleanup: remove logic and algebra.

--- sympycore/logic/sets/tests/test_set.py
+++
@@ -1,9 +0,0 @@
-
-from sympycore import *
-
-def xtest_sorting():
-    s1 = Set(1,2)
-    s2 = Set(3,4)
-    l = [s2, s1]
-    l.sort(cmp)
-    assert l==[s1,s2]
--- sympycore/logic/symbolic/tests/test_symbol.py
+++
@@ -1,7 +0,0 @@
-
-from sympycore import *
-
-def test_boolean():
-    b = Boolean('b')
-    assert b.tostr()=='b'
-    assert b.torepr()=="Boolean('b')"
