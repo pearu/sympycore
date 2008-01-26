a72c494c5c0f010192f39d84c13c90c0f0f8941e
pearu.peterson <pearu.peterson@eb797620-833c-0410-b191-3188e4ee2b90>
2008-01-26 21:25:16 +0000

    Fix calculus.Number to handle floats.

--- sympycore/calculus/__init__.py
+++ sympycore/calculus/__init__.py
@@ -3,7 +3,12 @@ from .algebra import Calculus, I, integrate, oo, undefined
 from .functions import exp, log, sqrt, sin, cos, tan, cot, pi, E
 
 Symbol = Calculus.Symbol
-Number = Calculus.Number
+
+def Number(num, denom=None):
+    n = Calculus.Number(Calculus.convert_coefficient(num))
+    if denom is None:
+        return n
+    return n / denom
 
 Add = lambda *args: Calculus.Add(*map(Calculus.convert, args))
 Mul = lambda *args: Calculus.Mul(*map(Calculus.convert, args))
