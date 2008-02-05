ca3add180e8dc124e9ebec35682215a6de0ae9b1
pearu.peterson <pearu.peterson@eb797620-833c-0410-b191-3188e4ee2b90>
2008-02-05 22:25:11 +0000

    Add test_poly_divide script.

---
+++ research/test_poly_divide.py
@@ -0,0 +1,46 @@
+
+# use time() instead on unix
+import sys
+if sys.platform=='win32':
+    from time import clock
+else:
+    from time import time as clock
+
+from sympycore import profile_expr
+
+def time1(n=500):
+    import sympycore as sympy
+    w = sympy.Fraction(3,4)
+    x = sympy.polynomials.poly([0, 1, 1])
+    a = (x-1)*(x-2)*(x-3)*(x-4)*(x-5)
+    b = (x-1)*(x-2)*(x-w)
+    t1 = clock()
+    while n:
+        divmod(a, b)
+        n -= 1
+    t2 = clock()
+    return 100 / (t2-t1)
+
+def time2(n=500):
+    import sympycore as sympy
+    w = sympy.Fraction(3,4)
+    x = sympy.polynomials.PolynomialRing[1]([0, 1, 1])
+    a = (x-1)*(x-2)*(x-3)*(x-4)*(x-5)
+    b = (x-1)*(x-2)*(x-w)
+    t1 = clock()
+    while n:
+        divmod(a, b)
+        n -= 1
+    t2 = clock()
+    return 100 / (t2-t1)
+
+def timing():
+    t1 = time1()
+    t2 = time2()
+    return t1, t2, t1/t2
+
+print timing()
+print timing()
+print timing()
+
+profile_expr('time2(50)')
