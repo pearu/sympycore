b5033284ea7ccb4e5e76983e474504a522b1bca8
fredrik.johansson <fredrik.johansson@gmail.com>
2008-02-10 19:00:08 +0000

    multi-case expand benchmark added

---
+++ bench/test_expand.py
@@ -0,0 +1,81 @@
+from sympycore import Symbol, Number
+x,y,z = map(Symbol,'xyz')
+
+bina1 = (3*x+2*y)**2
+binb1 = (3*x+2)**2
+binc1 = (3*x+2*y)**4
+
+bina2 = (Number(3,4)*x + Number(1,2)*y)**2
+binb2 = (Number(3,4)*x + Number(1,2))**2
+binc2 = (Number(3,4)*x + Number(1,2)*y)**4
+
+bin50a = (x+y)**50
+bin50b = (x+3)**50
+
+quad2 = (x+y+z+1)**2
+quad8 = (x+y+z+1)**8
+
+mixed1 = (x+1) * (y+1)
+mixed2 = (x+y) * (y+1)**2
+mixed3 = (x+y)**3 * (x+1)**2
+mixed4 = (x+y+1)**2 * (x+1)**2
+mixed5 = (x+y+3)**2 * (x+y+1)
+
+mixed6 = (x+y+1)**8 * (x+z)**4
+
+nested = (1+(x+2*z+(y+(1+y+z))**2)**2)
+
+def test_bin_1():
+    """Expand small binomials with int coeffs, 30x"""
+    for i in range(10):
+        bina1.expand()
+        binb1.expand()
+        binc1.expand()
+
+def test_bin_2():
+    """Expand small binomials with fractional coeffs, 30x"""
+    for i in range(10):
+        bina2 = (Number(3,4)*x + Number(1,2)*y)**2
+        binb2 = (Number(3,4)*x + Number(1,2))**2
+        binc2 = (Number(3,4)*x + Number(1,2)*y)**4
+
+def test_bin_3():
+    """Expand big binomials, 2x"""
+    bin50a.expand()
+    bin50b.expand()
+
+def test_quad_1():
+    """Expand small quadrinomial, 5x"""
+    for i in range(5):
+        quad2.expand()
+
+def test_quad_2():
+    """Expand large quadrinomial"""
+    quad8.expand()
+
+def test_mixed_1():
+    """Expand small mixed products, 8x"""
+    mixed1.expand(); mixed1.expand(); mixed1.expand()
+    mixed2.expand(); mixed2.expand(); 
+    mixed3.expand()
+    mixed4.expand()
+    mixed5.expand()
+
+def test_mixed_2():
+    """Expand large mixed product"""
+    mixed6.expand()
+
+def test_nested():
+    """Expand deeply nested expression"""
+    nested.expand()
+
+if __name__=='__main__':
+    from func_timeit import Timer
+    Timer(test_bin_1).smart_timeit()
+    Timer(test_bin_2).smart_timeit()
+    Timer(test_bin_3).smart_timeit()
+    Timer(test_quad_1).smart_timeit()
+    Timer(test_quad_2).smart_timeit()
+    Timer(test_mixed_1).smart_timeit()
+    Timer(test_mixed_2).smart_timeit()
+    Timer(test_nested).smart_timeit()
