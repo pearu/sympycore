dbecafbf7133ac4294e52f8b819be68654450374
pearu.peterson <pearu.peterson@eb797620-833c-0410-b191-3188e4ee2b90>
2008-02-11 20:18:02 +0000

    Added bench for matrix operations.

---
+++ bench/test_matrices.py
@@ -0,0 +1,38 @@
+from sympycore import MatrixRing
+
+a = MatrixRing[15,20].random()
+b = MatrixRing[15,20].random()
+
+c = MatrixRing[15,20].random((0,1))
+d = MatrixRing[15,20].random((0,1))
+
+def test_neg():
+    """Negate a 15x20 random dense matrix."""
+    -a
+
+def test_add():
+    """Add two 15x20 random dense matricies."""
+    a + b
+
+def test_sub():
+    """Substract two 15x20 random dense matricies."""
+    a - b
+
+def test_mul_coeff():
+    """Multiply 15x20 random dense matrix with a number."""
+    a * 3
+
+def test_div_coeff():
+    """Divide 15x20 random dense matrix with a number."""
+    a / 3
+
+def test_add_sparse():
+    """Add two 15x20 random sparse int matricies."""
+    c + d
+
+
+if __name__=='__main__':
+    from func_timeit import run_tests
+    run_tests([test_neg, test_add, test_sub, test_mul_coeff, test_div_coeff,
+               test_add_sparse])
+
