06be70b771972099ef92d12202374b35b48d0235
pearu.peterson <pearu.peterson@eb797620-833c-0410-b191-3188e4ee2b90>
2008-02-25 15:39:32 +0000

    Created benchmarks for integrate and diff.

--- bench/test_summation.py
+++ bench/test_summation.py
@@ -1,9 +1,9 @@
+START_REVISION=300
 
-
-from sympycore import Symbol
+from sympycore import Symbol, Add
 x,y,z = map(Symbol,'xyz')
 
-def test():
+def test_sum():
     """ sum(x**i/i,i=1..400)
     """
     s = 0
@@ -12,6 +12,12 @@ def test():
         s += x**i/i
         i -= 1
 
+def test_inplace_sum():
+    """ Add(x**i/i,i=1..400)
+    """
+    Add(*[x**i/i for i in xrange(1,401)])
+
 if __name__=='__main__':
-    from func_timeit import Timer
-    Timer(test).smart_timeit()
+    from func_timeit import run_tests
+    run_tests([test_sum, test_inplace_sum])
+
